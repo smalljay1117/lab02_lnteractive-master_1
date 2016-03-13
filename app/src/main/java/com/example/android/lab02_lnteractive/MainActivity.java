ca9e5df88403251b3a19bb9cef9ff3e74ef9bf3e
smalljay1117 <smalljay1117@gmail.com>
2016-03-13 23:41:09 +0800

    新增+-按鈕

--- app/src/main/java/com/example/android/lab02_lnteractive/MainActivity.java
+++ app/src/main/java/com/example/android/lab02_lnteractive/MainActivity.java
@@ -3,32 +3,49 @@ package com.example.android.lab02_lnteractive;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1,b2,b3;
+    TextView quantityTextView;
+    int qty = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button)findViewById(R.id.button);
+        b2 = (Button)findViewById(R.id.button2);
+        b3 = (Button)findViewById(R.id.button3);
+        quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
     }
 
     public void submitOrder(View view) {
-        display(2);
-
+        switch (view.getId()){
+            case R.id.button:
+                qty++;
+                quantityTextView.setText(qty + "");
+                break;
+            case R.id.button2:
+                qty--;
+                quantityTextView.setText(qty + "");
+                break;
+            case R.id.button3:
+                display();
+                break;
+        }
     }
 
-    private void display(int number) {
-        TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(String.valueOf(number));
-
+    private void display() {
         TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
         int price = 5;
-        int total = price * number;
-        String myString = NumberFormat.getCurrencyInstance().format(total);
+        int total = price * qty;
+        String myString = NumberFormat.getCurrencyInstance(Locale.TAIWAN).format(total);
         priceTextView.setText(myString);
-
     }
 }
